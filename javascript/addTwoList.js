d4e0077fa3878548765e5431480f9143451117bb
RomanLatipov <romanlatipov@gmail.com>
2024-07-24 21:38:49 -0400

    complete Add-two-numbers-list lab

--- javascript/addTwoList.js
+++ javascript/addTwoList.js
@@ -6,14 +6,36 @@ class Node {
 }
 
 function addTwoList(l1, l2) {
-// type your code here
+  let num1 = '';
+  let num2 = '';
+
+  while (l1 || l2) {
+    if(l1) {
+      num1 += l1.val;
+      l1 = l1.next;
+    }
+    if (l2) {
+      num2 += l2.val;
+      l2 = l2.next;
+    }
+  }
+  num1 = num1.split("").reverse().join("");
+  num2 = num2.split("").reverse().join("");
+  const sum = (parseInt(num1) + parseInt(num2)).toString();
+
+  let currentNode = null;
+  for(let i=0; i<sum.length; i++) {
+    const node = new Node(parseInt(sum[i]), currentNode);
+    currentNode = node;
+  }
+  
+  return currentNode;
 }
 
 if (require.main === module) {
 // add your own tests in here
 console.log("Expecting: { val: 0, next: null }");
-console.log("=>", addTwoNumbers({ val: 0, next: null }, { val: 0, next: null }));
-
+console.log("=>", addTwoList({ val: 0, next: null }, { val: 0, next: null }));
 }
 
 module.exports = {
